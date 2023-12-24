9fea035a5b424049ec1fcd1478b53fd515a8bd03
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-12-24 10:50:06 +0600

    created api to delete task from the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -40,6 +40,14 @@ async function run() {
     app.post("/api/user/task/create", async (req, res) => {
       const task = req.body;
       const result = await taskCollection.insertOne(task);
+      res.send(result);
+    });
+
+    // delete task
+    app.delete("/api/user/task/delete/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await taskCollection.deleteOne(query);
       res.send(result);
     });
 
